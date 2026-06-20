90845182e0ebd8c04a2799e0753639f16750a91e
Ayaz Farooqui <ayaz_farooqui@yahoo.com>
2026-06-19 22:41:14 -0700

    Add CS50P problem set solutions

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+name = input("What's Your Name? ")
+print("Hello,", name +"!", sep=" ")
+print(f"Hello, {name}" + "!")
+
+name = input("What's Your Name? ").strip().title()
+print("Hello,", name + "!")
+
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = round(x + y)
+
+print(f"{z:,}")
